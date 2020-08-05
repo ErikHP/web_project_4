b8688625088b009392122943c8f1e3e4e83d96a1
Erik Pulatov <epulatov@gmail.com>
2020-08-05 01:00:57 -0700

    Bugs are fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 
-const editFormButton = document.querySelector('.profile__button_edit');
+const editFormButton = document.querySelector('.profile__button_type_edit');
 const closeFormButton = document.querySelector('.modal__close-button');
 const modal = document.querySelector('.modal');
 const form = document.querySelector('.modal__form');
@@ -16,16 +16,18 @@ function toggleModal() {
     professionInput.value = profileProfession.textContent;
     modal.classList.toggle('modal_open');
 
-}
+};
 
 editFormButton.addEventListener('click', toggleModal);
 
-closeFormButton.addEventListener('click', toggleModal);
+closeFormButton.addEventListener('click', function closeModal(){
+    modal.classList.remove('modal_open');
+});
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     profileName.textContent = nameInput.value;
     profileProfession.textContent = professionInput.value;
-    toggleModal()
-})
+    toggleModal();
+});
 
