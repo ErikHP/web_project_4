555a71835708a4e05c6890f268d7581c121af724
Erik Pulatov <epulatov@gmail.com>
2020-09-27 14:43:04 -0700

    finish validation.js and modify index.html

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,5 +1,67 @@
+function showErrorMessage(input, form, {errorClass,inputErrorClass, ...rest}) {
+    const error = document.querySelector('#' + input.id + '-error');
+    error.textContent = input.validationMessage;
+
+    error.classList.add(errorClass);
+    input.classList.add(inputErrorClass);
+
+}
+
+function hideErrorMessage(input, form, {errorClass,inputErrorClass, ...rest}) {
+    const error = document.querySelector('#' + input.id + '-error');
+    error.textContent = '';
+
+    error.classList.remove(errorClass);
+    input.classList.remove(inputErrorClass);
+
+}
+
+
+
+function checkInputValidity(input, form, rest) {
+    if (input.validity.valid) {
+        //hideErrorMessage()
+        hideErrorMessage(input, form, rest);
+    } else {
+        //showErrorMessage()
+        showErrorMessage(input, form, rest);
+    }
+}
+
+function toggleButtonState(inputs, button, {inactiveButtonClass, ...rest}) {
+    const isValid = inputs.every((input) => input.validity.valid)
+    if (isValid) {
+        button.classList.remove(inactiveButtonClass);
+    } else {
+        button.classList.add(inactiveButtonClass);
+    }
+}
+
+function enableValidation({formSelector, inputSelector, submitButtonSelector,...rest}) {
+    const forms = [...document.querySelectorAll(formSelector)];
+
+    forms.forEach((form) => {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+        })
+
+        const inputs = [...form.querySelectorAll(inputSelector)];
+        const button = form.querySelector(submitButtonSelector);
+
+        inputs.forEach((input) => {
+            input.addEventListener('input', () => {
+                //checkInputValidity
+                checkInputValidity(input, form, rest);
+                //toggleButtonState
+                toggleButtonState(inputs, button, rest);
+            })
+        })
+    });
+}
+
+
 enableValidation({
-    formSelector: ".modal_form",
+    formSelector: ".modal__form",
     inputSelector: ".modal__input",
     submitButtonSelector: ".modal__save",
     inactiveButtonClass: "modal__save_disabled",
