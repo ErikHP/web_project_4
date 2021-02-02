8f85432e557e68c80e66556c65c85523fca1708f
Erik Pulatov <epulatov@gmail.com>
2021-02-01 21:07:54 -0600

    handleCardClick function fixed

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -8,10 +8,10 @@ export default class PopupWithImage extends Popup {
     this._modalImageTitle = document.querySelector('.modal__image-title');
   }
 
-  open() {
-    this._modalImage.src = this._link;
-    this._modalImage.alt = this._name;
-    this._modalImageTitle.textContent = this._name;
+  open({name, link}) {
+    this._modalImage.src = link;
+    this._modalImage.alt = name;
+    this._modalImageTitle.textContent = name;
 
     super.open();
   }
